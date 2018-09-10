2b10276081bd44e218a5d2a04e8fc1b26e0ff091
Joseph Biju <jbiju94@gmail.com>
2018-09-11 00:49:54 +0530

    no message

--- django_tut/blog_ster/api/models.py
+++ django_tut/blog_ster/api/models.py
@@ -6,8 +6,8 @@ def get_image_path(instance, filename):
 
 class Blog(models.Model):
 	title = models.CharField(max_length=255)
-	title_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	#tags = models.M
+	title_image = models.ImageField(upload_to='media', blank=True, null=True)
+	tags = models.ManyToManyField('Tag')
 	count_likes = models.AutoField()
 	author = models.ForeignKey(User)
 	pub_date = models.DateField(auto_now=False, auto_now_add=True)
@@ -17,15 +17,31 @@ class Blog(models.Model):
 	body = models.TextField()
 	#content_images = model
 
-class Comments:
+	def __str__(self):
+        return self.name
 
+class Comments:
+	content = models.TextField()
+	author = model.CharField(max_length=50, blank=True, null=True)
+	parent_blog = model.ForeignKey(Blog, on_delete=models.CASCADE)
+
+	def __str__(self):
+        return self.name
 
 class Tags:
+	name = model.CharField(max_length=50, blank=False, null=False)
 
+	def __str__(self):
+        return self.name
 
 class Media:
+	blog = models.ForeignKey(Blog)
+	image = models.ImageField(upload_to='media', blank=True, null=True)
+	url = model.CharField(max_length=256, blank=True, null=True)
 
-	
+	def __str__(self):
+        return self.name
+
 
 	
 	
